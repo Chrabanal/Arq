9fc00616f6426951359c5ba5f83dc1d17f72e90b
Elagentek <kevin.alexanderignacio@gmail.com>
2024-11-19 16:58:19 -0300

    backup recuperado

--- src/app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.stock.app',
-  appName: 'checkmystock',
-  webDir: 'www'
-};
-
-export default config;
